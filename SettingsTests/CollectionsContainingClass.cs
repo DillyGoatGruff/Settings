04e397b14b07d6e6a8d3611297f77d3f156c64c4
Dylan Schweiss <dschweiss@outlook.com>
2024-01-25 11:00:55 -0600

    include list tests.

---
+++ SettingsTests/CollectionsContainingClass.cs
@@ -0,0 +1,31 @@
+﻿using Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsTests
+{
+	internal class CollectionsContainingClass : SettingsBase<CollectionsContainingClass>
+	{
+
+		public List<Computer> Computers { get; set; } = new List<Computer>();
+
+
+		[Obsolete("Only used for serialization", true)]
+		public CollectionsContainingClass() { }
+
+		public CollectionsContainingClass(ISettingsSaver settingsSaver) : base(settingsSaver)
+		{
+
+		}
+	}
+
+
+	public class Computer
+	{
+
+		public string Name { get; set; }
+	}
+}
---
+++ SettingsTests/ListTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SettingsTests
+{
+	public class ListTests
+	{
+
+
+		[Fact]
+		public void Computer_Test()
+		{
+			//Arrange
+			CollectionsContainingClass settings = new CollectionsContainingClass(new InMemorySettingsSaver());
+
+			//Act
+			settings.Computers.Add(new Computer() { Name = "Test" });
+
+
+			//Assert
+			settings.CheckIsDirty().Should().BeTrue();
+		}
+
+	}
+}
