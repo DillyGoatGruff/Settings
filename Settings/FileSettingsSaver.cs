1b4325a6f3c81677512ab486792627211a4ce430
Dylan Schweiss <dschweiss@outlook.com>
2024-03-26 09:13:06 -0500

    Added xml documentation.

--- Settings/FileSettingsSaver.cs
+++ Settings/FileSettingsSaver.cs
@@ -1,9 +1,17 @@
 ﻿namespace Settings
 {
+    /// <summary>
+    /// An implementation of <see cref="ISettingsSaver"/> that stores the serialization in a file.
+    /// </summary>
     public class FileSettingsSaver : ISettingsSaver
     {
         private readonly string _filename;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filename">The full filename that the serialized data is saved in and loaded from.</param>
+        /// <remarks>If the file does not already exist, it will be created, along with any directories that are needed.</remarks>
         public FileSettingsSaver(string filename)
         {
             _filename = filename;
--- Settings/ISettingsSaver.cs
+++ Settings/ISettingsSaver.cs
@@ -1,13 +1,21 @@
 ﻿namespace Settings
 {
+    /// <summary>
+    /// Interface defining methods needed to save and load strings containing serialized data.
+    /// </summary>
     public interface ISettingsSaver
     {
         /// <summary>
         /// Gets the serialized settings.
         /// </summary>
-        /// <returns>The serialization of the settings. If unable to access the saved serialization, then <c>null</c>.</returns>
+        /// <returns>The serialization of the settings. If unable to access the saved serialization, then <see langword="null"/>.</returns>
         string? GetSavedSerialization();
 
+        /// <summary>
+        /// Saves the string.
+        /// </summary>
+        /// <param name="serialization">The serialized data.</param>
+        /// <returns><see langword="true"/> if successfully saved; else, <see langword="false"/>.</returns>
         bool SaveSerialization(string serialization);
     }
 }
